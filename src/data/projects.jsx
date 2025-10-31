467cbaa1b7c3f9cb1911de9a21fbc6e876ae2ed2
Horie Kyazimova <horish99@gmail.com>
2025-10-31 17:30:24 +0200

    Fix project image paths for Vercel

--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -1,8 +1,10 @@
 export const projects = [
-    {title: 'Horiflix',
-    description: "A modern and responsive movie and TV show browsing app built with React",
-    image: "./public/images/projects/horiflix.png",
+  {
+    title: "Horiflix",
+    description:
+      "A modern and responsive movie and TV show browsing app built with React.",
+    image: "/images/projects/horiflix.png",
     link: "https://horiflix-movie-app.vercel.app/",
-    sourceCode: "https://github.com/horishh/Horiflix-Movie-app"
-    }
-]
+    sourceCode: "https://github.com/horishh/Horiflix-Movie-app",
+  },
+];
